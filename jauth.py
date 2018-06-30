1f60bb08e32afdf0ae742a40b9ba7fedf178864e
Ralf Ren <me@ralf.ren>
2018-06-30 15:08:58 +0800

    All Done. auth & de-auth both work.

--- jauth.py
+++ jauth.py
@@ -9,7 +9,10 @@ import json
 from bs4 import BeautifulSoup
 
 __VERSION__ = '1.0.0'
-
+UA_STRING = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+            'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+            'Chrome/67.0.3396.87 ' \
+            'Safari/537.36'
 
 def bootstrap():
     parser = argparse.ArgumentParser(description='Joywise command-line authentication utility')
@@ -73,13 +76,13 @@ def do_connect(args):
 
     # Gather form data for authentication
     print('(2/5) Gathering form data for authentication...')
-    parser = BeautifulSoup(auth_page.text, 'html.parser')
+    parser_auth = BeautifulSoup(auth_page.text, 'html.parser')
 
-    data_mac = parser.select('input[id="mac"]')[0].get('value')
-    data_wlanacname = parser.select('input[id="wlanacname"]')[0].get('value')
-    data_url = parser.select('input[id="url"]')[0].get('value')
-    data_nasip = parser.select('input[id="nasip"]')[0].get('value')
-    data_wlanuserip = parser.select('input[id="wlanuserip"]')[0].get('value')
+    data_mac = parser_auth.select('input[id="mac"]')[0].get('value')
+    data_wlanacname = parser_auth.select('input[id="wlanacname"]')[0].get('value')
+    data_url = parser_auth.select('input[id="url"]')[0].get('value')
+    data_nasip = parser_auth.select('input[id="nasip"]')[0].get('value')
+    data_wlanuserip = parser_auth.select('input[id="wlanuserip"]')[0].get('value')
 
     data_username = ''
     data_pwd = ''
@@ -108,18 +111,22 @@ def do_connect(args):
         "wlanuserip": data_wlanuserip
     }
 
-    data_cookies = auth_page.cookies.get('JSESSIONID')
-
+    data_jsessionid = auth_page.cookies.get('JSESSIONID')
     cookies_data = {
-        'JSESSIONID': data_cookies,
+        'JSESSIONID': data_jsessionid,
         'failCounter': '0'
+    }
+
+    headers_data = {
+        'User-Agent': UA_STRING
     }
 
     # Sending authentication information
     print('(3/5) Sending authentication information...')
     host = rdr_url[7:].split('/')[0]
-    str_return = session.post('http://%s/zportal/login/do' % host, data=form_data, cookies=cookies_data)
-    json_return = json.loads(str_return.text)
+    auth_return = session.post('http://%s/zportal/login/do' % host,
+                               data=form_data, cookies=cookies_data, headers=headers_data)
+    json_return = json.loads(auth_return.text)
     if not json_return['result'] == 'success':
         print('----- Failed.')
         print(json_return)
@@ -127,23 +134,40 @@ def do_connect(args):
 
     # Extract session data
     print('(4/5) Extracting session data...')
+
+    data_userindex = auth_return.cookies.get('userIndex')
     cookies_next = {
-        'JSESSIONID': data_cookies,
+        'JSESSIONID': data_jsessionid,
         'username': data_username,
         'password': data_pwd,
         'rememberPassword': 'true',
         'failCounter': '0',
         'serviceId': '',
-        'userIndex': auth_page.cookies.get('userIndex')
+        'userIndex': data_userindex
     }
-    # TODO: Set referrer
-    session.get('http://%s/zportal/goToAuthResult' % host, cookies=cookies_next)
+
+    success_page = session.get('http://%s/zportal/goToAuthResult' % host, cookies=cookies_next)
+    parser_extract = BeautifulSoup(success_page.text, 'html.parser')
+
+    data_userip = parser_extract.select('input[name="userIp"]')[0].get('value')
+    data_deviceip = parser_extract.select('input[name="deviceIp"]')[0].get('value')
+    data_usermac = parser_extract.select('input[id="userMac"]')[0].get('value')
 
     # Saving current session
     print('(5/5) Saving current session...')
     ini = configparser.ConfigParser()
     ini.read(config_file)
-    ini['last'] = {'url': rdr_url}
+
+    ini['last'] = {
+        # 'url': rdr_url,
+        'host': host,
+        'jsessionid': data_jsessionid,
+        'userindex': data_userindex,
+        'userip': data_userip,
+        'deviceip': data_deviceip,
+        'usermac': data_usermac
+    }
+
     with open(config_file, 'w') as f:
         ini.write(f)
 
@@ -151,7 +175,8 @@ def do_connect(args):
     print('Connected.')
 
 
-def do_disconnect():
+def do_disconnect(args):
+
     # Check for Internet connection
     session = requests.Session()
     rdr = session.get('http://cdn.ralf.ren/res/portal.html').text
@@ -159,6 +184,56 @@ def do_disconnect():
         print("Internet is unavailable. No need to de-authenticate.")
         exit(3)
 
+    # Building de-authentication data
+    print('(1/2) Building de-authentication data...')
+    config_file = os.path.join(os.path.expanduser('~'), ".jauth_config")
+    if not os.path.exists(config_file):
+        print('Configuration file not exist.')
+        exit(4)
+
+    ini = configparser.ConfigParser()
+    ini.read(config_file)
+
+    form_data = {
+        "userName": ini.get('account', 'username'),
+        "userIp": ini.get('last', 'userip'),
+        "deviceIp": ini.get('last', 'deviceip'),
+        "service.id": "",
+        "autoLoginFlag": "false",
+        "userMac": ini.get('last', 'usermac'),
+        "operationType": "",
+        "isMacFastAuth": "false"
+    }
+
+    cookies_data = {
+        'JSESSIONID': ini.get('last', 'jsessionid'),
+        'username': ini.get('account', 'username'),
+        'password': ini.get('account', 'password'),
+        'rememberPassword': 'true',
+        'failCounter': '0',
+        'serviceId': '',
+        'userIndex': ini.get('last', 'userindex')
+    }
+
+    headers_data = {
+        'User-Agent': UA_STRING
+    }
+
+    # Sending de-authentication data
+    print('(2/2) Sending de-authentication data...')
+    session = requests.Session()
+    deauth_return = session.post('http://%s/zportal/logout' % ini.get('last', 'host'),
+                                 data=form_data, cookies=cookies_data, headers=headers_data)
+
+    parser_deauth = BeautifulSoup(deauth_return.text, 'html.parser')
+    login_button_text = parser_deauth.select('button[id="relogin"]')[0].text.strip()
+
+    if not login_button_text == '继续上网':
+        print('----- Failed.')
+        exit(5)
+
+    # Done.
+    print('Disconnected.')
 
 
 if __name__ == '__main__':
